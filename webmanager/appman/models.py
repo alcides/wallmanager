b3a3522b5a9ef284f7c3b476023716e8157c7174
Alcides Fonseca <me@alcidesfonseca.com>
2010-03-31 12:44:09 +0100

    Refactored Models with tests. Tests already performed by django were removed.

--- webmanager/appman/models.py
+++ webmanager/appman/models.py
@@ -1,33 +1,64 @@
 from django.db import models
 
 class User(models.Model):
-	username = models.EmailField()
-	usertype = models.CharField(max_length=12)
-	isbanned = models.BooleanField()
-	
+    email = models.EmailField()
+    is_admin = models.BooleanField(default=False)
+    
+    def __unicode__(self):
+        return u"%s" % self.email
+    
 class Category(models.Model):
-	name = models.CharField(max_length=255)
-	
+    name = models.CharField(max_length=255)
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
 class Application(models.Model):
-	name = models.CharField(max_length=255)
-	appowner = models.ForeignKey('User')
-	category = models.ForeignKey('Category')
-	like = models.IntegerField()
-	dislike = models.IntegerField()
-	zipfile = models.FileField(upload_to='applications')
-	icon = models.FilePathField()
-	extractionpath = models.FilePathField()
-	
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User)
+    category = models.ForeignKey(Category)
+    
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+    
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    zipfile = models.FileField(upload_to='applications')
+    icon = models.FileField(upload_to='icons')
+    extraction_path = models.FilePathField()
+    
+    def value(self):
+        """ The value of an application, based on the likes and disliked """
+        return self.likes - self.dislikes
+        
+    class Meta:
+        ordering = ("-likes",)
+    
+    def __unicode__(self):
+        return u"%s" % self.name
+    
+    
 class AbuseReport(models.Model):
-	appid = models.ForeignKey('Application')
-	reportedby = models.ForeignKey('User')
-	description = models.TextField()
-	
-class WallManager(models.Model):
-	contact = models.EmailField()
-	superadmin = models.IntegerField()
-	
+    application = models.ForeignKey(Application)
+    author = models.ForeignKey(User)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return u"%s reported by %s" % (self.application, self.author)
+    
+    
+class AuthConfig(models.Model):
+    contact = models.EmailField()
+    superadmin = models.IntegerField()
+    
+    class Meta:
+        verbose_name = "Global Configuration"
+        verbose_name_plural = "Global Configurations"
+    
 class ProjectorControl(models.Model):
-	inactivitytime = models.IntegerField()
-	shutdowntime = models.TimeField()
-	startuptime = models.TimeField()
+    inactivity_time = models.IntegerField()
+    shutdown_time = models.TimeField()
+    startup_time = models.TimeField()
