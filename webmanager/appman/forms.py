de7be93c0d6517ad577cf03d5555b82baefb2c00
Joao Rodrigues <joao.marques.rodrigues@gmail.com>
2010-04-06 16:58:30 +0100

    Forms.py added and ApplicationForm was created

---
+++ webmanager/appman/forms.py
@@ -0,0 +1,7 @@
+from appman.models import Application
+from django.forms import ModelForm
+
+class ApplicationForm(ModelForm):
+	class Meta:
+		model = Application
+		fields = ('name', 'zipfile','icon','category')
--- webmanager/appman/views.py
+++ webmanager/appman/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.list_detail import *
 from django.views.generic.create_update import *
 
+from appman.forms import *
 from appman.models import *
 
 def application_list(request):
@@ -8,4 +9,4 @@ def application_list(request):
 	return object_list(request, queryset=cs, template_object_name="application")
 	
 def application_form(request):
-	return create_object(request, model=Application,post_save_redirect="/app/list/")
+	return create_object(request, form_class=ApplicationForm, post_save_redirect="/app/list/")
