1abe5aa2bdaf98907f455ac510a54a920a0d81e6
Alcides Fonseca <me@alcidesfonseca.com>
2010-04-06 22:40:50 +0100

    Added tests to edit view

--- webmanager/appman/tests.py
+++ webmanager/appman/tests.py
@@ -9,14 +9,22 @@ class ApplicationManagement(TestCase):
     
     def setUp(self):
         #Creates usernames for Zacarias and Prof. Plum
-        self.zacarias = User.objects.create(username="zacarias_stu", email="zacarias@student.dei.uc.pt")
-        self.plum = User.objects.create(username="plum_ede", email="plum@dei.uc.pt", is_staff=True, is_superuser=True)
-		
+        self.zacarias = User.objects.create_user(username="zacarias_stu", email="zacarias@student.dei.uc.pt", password="zacarias")
+        self.plum = User.objects.create_user(username="plum_ede", email="plum@dei.uc.pt", password="plum")
+        self.plum.is_staff = True
+        self.plum.is_superuser = True
+        self.plum.save()
+        		
         self.educational = Category.objects.create(name="Educational")
         self.games = Category.objects.create(name="Games")
         
         self.gps = Application.objects.create(name="Gps Application", owner=self.plum, category=self.educational)
-    
+ 
+    def do_login(self):
+        login = self.client.login(username='zacarias_stu', password='zacarias')
+        self.assertEqual(login, True)
+        return login
+   
     def test_models_representation(self):
         self.assertEqual( unicode(self.educational), u"Educational" )
         self.assertEqual( unicode(self.gps), u"Gps Application" )
@@ -38,6 +46,35 @@ class ApplicationManagement(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "<td>Gps Application</td>")
         self.assertContains(response, "<tr>", 2) # 1 app, plus header
+
+    def test_requires_login_to_add_app(self):
+        response = self.client.get('/app/add/')
+        self.assertEqual(response.status_code, 302) # redirect to login
+        self.assertRedirects(response, '/accounts/login/?next=/app/add/')
+    
+    def test_add_app(self):
+        login = self.do_login()
+        response = self.client.get('/app/add/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<form", 1)
+
+        zf = open('requirements.txt')
+        pf = open('tests/wmlogo.png')        
+        post_data = {
+            'name': 'Example App',
+            'zipfile': zf,
+            'icon': pf,
+            'category': self.educational.id, 
+            'description': "Example app"
+        }
+        response = self.client.post('/app/add/', post_data)
+        zf.close()
+        pf.close()
+        
+        self.assertRedirects(response, '/app/list/')
+        response = self.client.get('/app/list/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<td>Example App</td>")
 
 
     def tearDown(self):
