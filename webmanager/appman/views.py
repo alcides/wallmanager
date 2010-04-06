2bfd4a0b7048555be709c3f90da2f15fa8af45cb
Joao Rodrigues <joao.marques.rodrigues@gmail.com>
2010-04-06 17:59:02 +0100

    Add application is working with a test user

--- webmanager/appman/views.py
+++ webmanager/appman/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.list_detail import *
 from django.views.generic.create_update import *
+from django.shortcuts import render_to_response
 
 from appman.forms import *
 from appman.models import *
@@ -9,4 +10,22 @@ def application_list(request):
 	return object_list(request, queryset=cs, template_object_name="application")
 	
 def application_form(request):
-	return create_object(request, form_class=ApplicationForm, post_save_redirect="/app/list/")
+	form_class = ApplicationForm
+	if request.method == 'POST':
+		form = form_class(request.POST, request.FILES)
+		if form.is_valid():
+			app = form.save(commit=False)
+			
+			try:
+				app.owner = User.objects.get(email="teste@teste.com")
+			except:
+				app.owner = User.objects.create(email="teste@teste.com",level='U')
+				
+			app.save()
+			return HttpResponseRedirect('/app/list/') # Redirect after POST
+	else:
+		form = form_class() # An unbound form
+
+	return render_to_response('appman/application_form.html', {
+		'form': form,
+	})
