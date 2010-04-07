4d617456f394697c626043da33b0ea68d4017aa3
Alcides Fonseca <me@alcidesfonseca.com>
2010-04-07 12:17:38 +0100

    Refactored default screen size, and fixed the default height.

--- mtmenu/ui/mainwindow.py
+++ mtmenu/ui/mainwindow.py
@@ -3,13 +3,13 @@ from pymt import *
 __all__ = ['MainWindow']
 
 class MainWindow (MTWindow):
-    """Menu main window
+    """Menu main window"""
     
-    Default width: 1024 * 2
-    Default height: 748 * 2
-    """
+    DEFAULT_WIDTH = 2 * 1024
+    DEFAULT_HEIGHT = 748
+    
     def __init__(self, **kwargs):
-        kwargs.setdefault('width', 1024 * 2)
-        kwargs.setdefault('height', 748 * 2)
+        kwargs.setdefault('width', DEFAULT_WIDTH)
+        kwargs.setdefault('height', DEFAULT_HEIGHT)
         
         super(kwargs)
