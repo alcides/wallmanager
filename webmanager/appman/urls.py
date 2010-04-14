54afde6d6e3af03a3bf778c6cd8ee72ec0d3daee
Alcides Fonseca <me@alcidesfonseca.com>
2010-04-14 12:00:00 +0100

    Changed urls to be more readable

--- webmanager/appman/urls.py
+++ webmanager/appman/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls.defaults import *
 from appman.models import *
 
 urlpatterns = patterns('appman.views',
-	url(r'^app/list/$', 'application_list', name="application-list"),
-	url(r'^app/add/$', 'application_add', name="application-add"),
-	url(r'^app/(?P<object_id>\d+)/$', 'application_detail', name="application-detail"),
-	url(r'^app/(?P<object_id>\d+)/edit/$', 'application_edit', name="application-edit"),
-	url(r'^app/(?P<object_id>\d+)/delete/$', 'application_delete', name="application-delete"),
-	url(r'^app/(?P<object_id>\d+)/remove/$', 'application_admin_remove', name="application-admin-remove"),
-	url(r'^cat/list/$', 'category_list', name="category-list"),
-	url(r'^cat/(?P<object_id>\d+)/edit/$', 'category_edit', name="category-edit"),
-	url(r'^cat/add/$', 'category_add', name="category-add"),
-	url(r'^cat/(?P<object_id>\d+)/remove/$', 'category_remove', name="category-remove")
+	url(r'^applications/$', 'application_list', name="application-list"),
+	url(r'^applications/add/$', 'application_add', name="application-add"),
+	url(r'^applications/(?P<object_id>\d+)/$', 'application_detail', name="application-detail"),
+	url(r'^applications/(?P<object_id>\d+)/edit/$', 'application_edit', name="application-edit"),
+	url(r'^applications/(?P<object_id>\d+)/delete/$', 'application_delete', name="application-delete"),
+	url(r'^applications/(?P<object_id>\d+)/remove/$', 'application_admin_remove', name="application-admin-remove"),
+	url(r'^categories/$', 'category_list', name="category-list"),
+	url(r'^categories/(?P<object_id>\d+)/edit/$', 'category_edit', name="category-edit"),
+	url(r'^categories/add/$', 'category_add', name="category-add"),
+	url(r'^categories/(?P<object_id>\d+)/remove/$', 'category_remove', name="category-remove")
 )
