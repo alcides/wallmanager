8b67a7ab9eae5a24b7a70ad9b31c08110629ec6b
Alcides Fonseca <me@alcidesfonseca.com>
2010-04-20 00:53:16 +0100

    Fixed tests related to relative paths.

--- webmanager/appman/tests/__init__.py
+++ webmanager/appman/tests/__init__.py
@@ -86,8 +86,8 @@ class ApplicationManagementTest(TestCase):
         self.assertContains(response, "Add Application")
         self.assertContains(response, "<form", 1)
 
-        zf = open(relative('../tests/python_test_app.zip'),'rb')
-        pf = open(relative('../tests/wmlogo.png'),'rb')
+        zf = open(relative('../../tests/python_test_app.zip'),'rb')
+        pf = open(relative('../../tests/wmlogo.png'),'rb')
         post_data = {
             'name': 'Example App',
             'zipfile': zf,
@@ -115,8 +115,8 @@ class ApplicationManagementTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Edit Application")
         
-        zf = open(relative('../tests/python_test_app.zip'),'rb')
-        pf = open(relative('../tests/wmlogo.png'),'rb')
+        zf = open(relative('../../tests/python_test_app.zip'),'rb')
+        pf = open(relative('../../tests/wmlogo.png'),'rb')
         post_data = {
             'name': 'Example App 2',
             'zipfile': zf,
@@ -142,9 +142,9 @@ class ApplicationManagementTest(TestCase):
         self.assertEqual(c-1, Application.objects.count())
     
     def test_email_after_zip_extraction(self):
-        file = File(relative("../tests/python_test_app.zip"))
+        file = File(relative("../../tests/python_test_app.zip"))
         app = Application.objects.create(name="E-mail Testing", owner=self.zacarias, category=self.educational, zipfile=file)
-        thread = UncompressThread(Application, app, "../tests/temp")
+        thread = UncompressThread(Application, app, "../../tests/temp")
         thread.run()
         self.assertEquals(len(mail.outbox), 1)
         #Is the message correct?
