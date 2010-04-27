ff50d7b5372566ed7f0610006e6fc9c1d8a28fa3
diogo machado <diogo@diogo-laptop.(none)>
2010-04-27 15:46:11 +0100

    Double tap implemented in application button.

--- mtmenu/ui/appbutton.py
+++ mtmenu/ui/appbutton.py
@@ -1,5 +1,5 @@
 from pymt import *
-
+from pymt.input.postproc.doubletap import InputPostprocDoubleTap
 
 class AppButton(MTKineticItem):
     """Widget representing an application on main window. 
@@ -10,8 +10,6 @@ class AppButton(MTKineticItem):
     
     This widget should be added to AppsGrid on Menu's UI construction"""
     def __init__(self, app, **kwargs):
-        self.app = app
-
         kwargs.setdefault('label', unicode(app))
         kwargs.setdefault('deletable', False)
         kwargs.setdefault('anchor_x', 'center')
@@ -19,14 +17,23 @@ class AppButton(MTKineticItem):
         kwargs.setdefault('halign', 'center')
         kwargs.setdefault('valign', 'middle')
         kwargs.setdefault('size', (200,200))        
+        
+        self.app = app
         super(AppButton, self).__init__(**kwargs)
 
-        
 
     """Execute application on click"""
     def on_press( self, touch ):
+        if touch.is_double_tap:
+            self.handle_double_tap()
+        else:
+            return
+        
+        
+    def handle_double_tap(self):
         print '\nLoading %s...\n' % unicode(self.app)
         print 'ID: %i' % self.app.id
         print '\tPath: %s\n' % self.app.get_extraction_fullpath
         print '\tBoot file: %s\n' % self.app.get_boot_file()
         self.app.execute()
+
