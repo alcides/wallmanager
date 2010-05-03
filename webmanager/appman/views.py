b66eb79ef2c0b977ff1c020c2630eded2053a0c3
Miguel Martins <Mr_Miguel40@hotmail.com>
2010-05-03 19:08:18 +0100

    Define contact admin is now functional.

--- webmanager/appman/views.py
+++ webmanager/appman/views.py
@@ -145,7 +145,13 @@ def manage_admins(request):
 @superuser_required()
 def contact_admin(request):
     admin_list = User.objects.filter(is_staff = True).exclude(is_superuser = True)
-    return render(request,'appman/contact_admin.html',{'admin_list': admin_list})
+    current_contact_admin = WallManager.objects.all()[0].contact
+    if request.method == 'POST':
+        contact_admin = request.POST['contact_admin']
+        WallManager.objects.create(contact = contact_admin)
+        current_contact_admin = contact_admin
+        request.user.message_set.create(message="The contact admin was defined successfully.")
+    return render(request,'appman/contact_admin.html',{'admin_list': admin_list, 'current_contact_admin': current_contact_admin})
 
 @staff_login_required
 def category_list(request):
