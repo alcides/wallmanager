a2bd9bc0cb12d3b6449bf0eab2f59d36bbb35505
Joao Rodrigues <joao.marques.rodrigues@gmail.com>
2010-05-10 17:30:08 +0100

    Projectors form created

--- webmanager/appman/forms.py
+++ webmanager/appman/forms.py
@@ -37,7 +37,10 @@ class DocumentationForm(ModelForm):
     class Meta:
         model = FlatPage
         fields = ('title','content')
-    
+        
+class ProjectorControlForm(ModelForm):
+    class Meta:
+        model = ProjectorControl
 
 class UserCreationForm(ModelForm):
     """
