64fd8780acab77fb368ee601b8ba004b702d511c
Joao Rodrigues <joao.marques.rodrigues@gmail.com>
2010-05-15 17:21:44 +0100

    bug fixed in threading

--- webmanager/appman/utils/proj_connection.py
+++ webmanager/appman/utils/proj_connection.py
@@ -5,10 +5,10 @@ import threading
 class ProjectorsThread(threading.Thread):
 	""" Thread that makes the conncetion to the rojectors."""
 	def __init__(self,proj):
-		threading.Thread.__init__(self)
 		times = map(self.to_array,[proj.startup_week_time,proj.shutdown_week_time,
 				proj.startup_weekend_time,proj.shutdown_weekend_time])
 		self.week_on, self.week_off, self.weekend_on, self.weekend_off = times
+		threading.Thread.__init__(self)
 
 	def to_array(self,date):
 		return [date.hour,date.minute]
